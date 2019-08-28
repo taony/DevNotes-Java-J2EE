ebf19162a7075e016e09ae3101990385d4e5b3b4
taony <taonyzhang@gmail.com>
2019-08-28 23:40:17 +0800

    commit flowable

---
+++ flowable/flowable-demo/src/main/java/com/taony/flowable/controller/FlowTestController.java
@@ -0,0 +1,16 @@
+package com.taony.flowable.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @description: TODO
+ * @author: taony
+ * @email:taonyzhang@gmail.com
+ * @date: 2019-08-28 16:01
+ */
+@Controller
+@RequestMapping("/flow")
+public class FlowTestController {
+
+}
