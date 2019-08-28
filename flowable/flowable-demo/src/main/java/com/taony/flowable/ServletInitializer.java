4ef894cff71d71cbf4ef91285597a1d470b1d73f
taony <taonyzhang@gmail.com>
2019-08-28 23:41:27 +0800

    commit flowable

---
+++ flowable/flowable-demo/src/main/java/com/taony/flowable/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.taony.flowable;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(FlowableDemoApplication.class);
+    }
+
+}
